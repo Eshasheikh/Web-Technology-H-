1c2d8008c2eb6308fff1de3ba35b0148c5887462
Eshasheikh <49494337+Eshasheikh@users.noreply.github.com>
2020-10-18 11:57:48 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,49 @@
+<meta charset ="UTF-8">
+			<meta name ="viewport" content="device-width",initial-scale=1.0"
+			</head>
+			
+			
+			<body>			
+			<form action="loginaction/result.php" method="post">			
+			<fieldset>			
+			<legend align="center"> Registration: </legend>
+			
+			<label for="fname"> Name:</label>
+			  <input type ="text" name="name" id="name"  ><br/><br/>
+			  
+			 Email : <input type="text" name="Email"><br/><br/>
+			 User Name : <input type="text" name="Username"><br/><br/>
+			 Password : <input type="text" name="Password"><br/><br/>
+			  Confirm Password : <input type="text" name="Confirm Password"><br/><br/>
+			 
+			 
+		<form action="/action.php" method="post">			
+			<fieldset
+			<legend align="center"> Gender: </legend>
+			<input type="radio" id="male" name="gender" value="male">
+			<label for="male">Male</label>
+			<input type="radio" id="female" name="gender" value="female">
+			<label for="female">Female</label>
+			<input type="radio" id="other" name="other" value="mail">
+			<label for="other">Other</label><br/><br/>
+			<input type="checkbox" id="one" name="a" value="html">
+			
+			<form action="/action.php" method="post">			
+			<fieldset>
+			<legend align="center"> Date of Birth: </legend>
+			
+			Day:<input type="Day">
+			
+			Month:<input type= "Month">
+			
+			Year: <input type="Year"><br/><br/>
+			
+		
+
+			<input type="submit" value="Submit">
+			
+			<input type="Reset" value="Reset">
+			</fieldset>
+			 </form>
+			</body>
+			</html>
